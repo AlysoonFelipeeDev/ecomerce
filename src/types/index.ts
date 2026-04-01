e0148500d364119ed1e3e286576fb9446b850087
jean <alysoonfelipe@hotmail.com>
2026-04-01 13:23:16 -0300

    feat: atualiza interface Product com imageUrl e category

--- src/types/index.ts
+++ src/types/index.ts
@@ -2,8 +2,8 @@ export interface Product  {
     id: string
     name: string
     price: number
-    image: string
-    description: string
+    imageUrl: string
+    category: string
 }
 
 export interface CartItem extends Product {
