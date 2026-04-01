b80a2b882023877e805bcacccb505bbea3ac51db
jean <alysoonfelipe@hotmail.com>
2026-04-01 14:55:10 -0300

    feat: adiciona interface ApiProduct para tipagem da DummyJSON

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,11 +1,19 @@
 export interface Product  {
     id: string
     name: string
-    price: number
+    price: string
     imageUrl: string
     category: string
 }
 
 export interface CartItem extends Product {
     quantity: number
+}
+
+export interface ApiProduct {
+    id: string,
+    title: string,
+    price: string,
+    thumbnail: string,
+    category: string
 }
