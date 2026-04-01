d081b6ed2720308b75dd841b4b611e0e69a32831
jean <alysoonfelipe@hotmail.com>
2026-04-01 15:55:58 -0300

    feat: adiciona rota principal para página Home

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,7 @@
-import { ProductCard } from "./components/ProductCard"
-import type { Product } from "./types"
-
+import { Home } from "./pages/Home"
 
 function App() {
-  const mockProduct: Product =
-    {
-      id: '1',
-      imageUrl: 'https://img.ltwebstatic.com/gspCenter/goodsImage/2022/8/25/7749784291_1018138/ED5B8C0462F156EE5F56026A044CC797.jpg',
-      category: "Moda Masculina",
-      name: 'Calça Preta',
-      price: 59.90,
-    }
-    
-  return <ProductCard 
-  id={mockProduct.id} name={mockProduct.name} category={mockProduct.category} price={mockProduct.price} imageUrl={mockProduct.imageUrl}
-  />
+  return <Home />
 }
 
 export default App
